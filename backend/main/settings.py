4fa361690d44f8fe5ecf1ab1e146384e48aaec76
mostafa <mustashrf@gmail.com>
2024-01-12 18:03:16 +0200

    restructured the project

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
-# from dotenv import load_dotenv
-# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
