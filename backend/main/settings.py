7e4c405461b8a6b627ef6f8d26077c844703d402
mostafa <mustashrf@gmail.com>
2024-01-17 02:26:26 +0200

    increased access_token lifetime

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -72,7 +72,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
