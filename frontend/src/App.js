69df41cd9fe0a6e4c9ab2ecbb2bf70c1e44d0a94
mostafa <mustashrf@gmail.com>
2024-01-17 03:23:35 +0200

    added authentication check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import AccountForm from './components/Account/AccountForm';
+import AccountForm from './components/Account/AccountForm'
+import Home from './components/Home/Home';
+import AuthService from './Services/auth.service';
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user && user.access_token) {
+      setIsAuthenticated(true);
+    }
+  }, [isAuthenticated]);
+
   return (
     <div className="App">
-      <AccountForm />
+      <div className="container">
+        {console.log(isAuthenticated)}
+        {isAuthenticated ? <Home /> : <AccountForm setIsAuthenticated={setIsAuthenticated}/>}
+      </div>
     </div>
   );
 }
